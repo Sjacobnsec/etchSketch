c1204b19bd2b1d854a32856a3116bdf4f8026633
Nsec <S.jacobnsec@gmail.com>
2015-08-18 14:07:40 -0500

    script notes

--- application.js
+++ application.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
-	
+	//make divs appear as a grid
+	//hover effect, mouseenter() and mouseleave(), change color
+	//should have a trailing effect, clear should be slow...
 
+	//button should reset page and pop dialogue asking for new grid amount
 });
