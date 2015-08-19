3504d66fa921fcac67f4c4e2d44d7233fe4ed386
Nsec <S.jacobnsec@gmail.com>
2015-08-19 12:52:37 -0500

    using append() to add divs

--- application.js
+++ application.js
@@ -1,22 +1,10 @@
+var row = 8;
+var column = 8; 
+
+
 $(document).ready(function() {
-	//make divs appear as a grid
-	//hover effect, mouseenter() and mouseleave(), change color
-	//should have a trailing effect, clear should be slow...
-
-	//button should reset page and pop dialogue asking for new grid amount
-
-	$('div').on('click', function () {
-    	$('div').addClass("highlight");
-	});
-
-	$('div').mouseleave(function () {
-    $(this).animate({height: "120px"}, 400);
-    $(this).toggleClass('highlight');
-	});
-	
-	$('div').mouseenter(function () {
-    $(this).animate({height: "20px"}, 400);
-    $(this).toggleClass('highlight');
-	});
-
+	for (var i = 0; i < row; i++) {
+		//appends 8 divs to wrapper 
+		$('#wrapper').append($('<div class="app"></div>'));
+	}
 });
