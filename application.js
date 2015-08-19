848bffc2bae07f0e52acc9b39962b8b35f7c2d52
Nsec <S.jacobnsec@gmail.com>
2015-08-18 20:07:04 -0500

    making the divs work

--- application.js
+++ application.js
@@ -8,4 +8,15 @@ $(document).ready(function() {
 	$('div').on('click', function () {
     	$('div').addClass("highlight");
 	});
+
+	$('div').mouseleave(function () {
+    $(this).animate({height: "120px"}, 400);
+    $(this).toggleClass('highlight');
+	});
+	
+	$('div').mouseenter(function () {
+    $(this).animate({height: "20px"}, 400);
+    $(this).toggleClass('highlight');
+	});
+
 });
