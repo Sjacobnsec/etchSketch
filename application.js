839a69e008be336497321a881deb383778183576
Sam <s.jacobnsec@gmail.com>
2015-08-20 18:10:22 -0500

    refactor

--- application.js
+++ application.js
@@ -1,21 +1,16 @@
-
-var row = 1000;
-var column = 8; 
-
+var row = 64;
+var column = 8;
 
 $(document).ready(function() {
-	for (var i = 0; i < row; i++) {
-		//appends 8 divs to wrapper 
-		$('#wrapper').after($('<div class="app"></div>'));
-	}
- /* $('.app').mouseenter(function(){
-    $(this).addClass('highlight');
-  }); */
-  
+  for (var i = 0; i < row; i++) {
+    //appends 8 divs to wrapper 
+    $('#wrapper').after($('<div class="app"></div>'));
+  }
+
   $('button').on('click', function() {
     $('.app').removeClass('highlight');
   });
-   $('.app').hover(function() {
-    $(this).toggleClass('highlight');
+  $('.app').hover(function() {
+    $(this).addClass('highlight');
   });
 });
