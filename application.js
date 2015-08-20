8fb5362726e85c6687e2f67393e2cfe4c3add203
Sam <s.jacobnsec@gmail.com>
2015-08-20 18:54:34 -0500

    Update application.js

--- application.js
+++ application.js
@@ -2,14 +2,14 @@ var row = 64;
 var column = 8;
 
 $(document).ready(function() {
+   $('button').on('click', function() {
+    $('.app').removeClass('highlight');
+  });
+  
   for (var i = 0; i < row; i++) {
     //appends 8 divs to wrapper 
     $('#wrapper').after($('<div class="app"></div>'));
   }
-
-  $('button').on('click', function() {
-    $('.app').removeClass('highlight');
-  });
   $('.app').hover(function() {
     $(this).addClass('highlight');
   });
