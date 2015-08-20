78ec035134b5b2c680487afcd34886b34732c64b
Sam <s.jacobnsec@gmail.com>
2015-08-20 15:53:05 -0500

    grid creation, highlighting
    Using after(), before(), append() to find the correct answer. Also used this to target only the div that is currently being 'moused' over.

--- application.js
+++ application.js
@@ -1,10 +1,17 @@
-var row = 8;
+
+var row = 50;
 var column = 8; 
 
 
 $(document).ready(function() {
 	for (var i = 0; i < row; i++) {
 		//appends 8 divs to wrapper 
-		$('#wrapper').append($('<div class="app"></div>'));
+		$('#wrapper').after($('<div class="app"></div>'));
 	}
+  $('.app').mouseenter(function(){
+    $(this).addClass('highlight');
+  });
+  $('.app').mouseleave(function(){
+    $(this).removeClass('highlight');
+  });
 });
