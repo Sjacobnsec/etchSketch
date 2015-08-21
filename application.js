e20453136427a0c405adc2b96e7eec517b2eea83
Sam <s.jacobnsec@gmail.com>
2015-08-21 16:33:06 -0500

    up against wall

--- application.js
+++ application.js
@@ -1,16 +1,21 @@
-var row = 64;
+var row = 5164;
 var column = 8;
+var cont = $("#container")
 
 $(document).ready(function() {
    $('button').on('click', function() {
     $('.app').removeClass('highlight');
   });
   
-  for (var i = 0; i < row; i++) {
-    //appends 8 divs to wrapper 
-    $('#wrapper').after($('<div class="app"></div>'));
+  for (var i = 0; i < cont.height(); i++) {
+  
+    $('#wrapper').append($('<div class="app"></div>'));
   }
   $('.app').hover(function() {
     $(this).addClass('highlight');
   });
 });
+
+console.log(cont.height())
+console.log($('.app').length)
+console.log("kek")
